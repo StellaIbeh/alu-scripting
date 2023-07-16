d8e831ca5c0666351d18d61966fbbc114e4918ef
StellaIbeh <a.ibeh@alustudent.com>
2023-07-16 19:26:59 +0000

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,50 +1,14 @@
 #!/usr/bin/python3
+"""
+3-main
+"""
+import sys
 
-"""search post function"""
-
-import json
-import operator
-import requests
-
-def count_words(subreddit, word_list, after=None, counts=None):
-    if counts is None:
-        counts = {}
-    
-    if not word_list:
-        print_counts(counts)
-        return
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    params = {"after": after} if after else {}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    if response.status_code != 200:
-        print_counts(counts)
-        return
-    
-    data = response.json()
-    children = data["data"]["children"]
-    for child in children:
-        title = child["data"]["title"].lower()
-        count_keywords(title, word_list, counts)
-    
-    after = data["data"]["after"]
-    if after:
-        count_words(subreddit, word_list, after, counts)
+if __name__ == '__main__':
+    count_words = __import__('3-count').count_words
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(
+            sys.argv[0]))
     else:
-        print_counts(counts)
-
-
-def count_keywords(title, word_list, counts):
-    words = title.split()
-    for word in words:
-        word = word.strip('.,!?_')
-        if word.lower() in word_list:
-            counts[word.lower()] = counts.get(word.lower(), 0) + 1
-
-
-def print_counts(counts):
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-    for word, count in sorted_counts:
-        print(f"{word}: {count}")
+        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
