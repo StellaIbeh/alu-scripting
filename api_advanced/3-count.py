b6bf6c85c6ca220fb01de6e5dc4af769102dd160
StellaIbeh <a.ibeh@alustudent.com>
2023-07-16 19:24:36 +0000

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -6,67 +6,45 @@ import json
 import operator
 import requests
 
-
-def count_words(subreddit, word_list, after=None):
-    """get all the keyword count"""
-
-    if len(word_list) == 0:
-        print(None)
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = {}
+    
+    if not word_list:
+        print_counts(counts)
         return
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
-    result = requests.get(url,
-                          headers=headers,
-                          params={"after": after},
-                          allow_redirects=False)
-    if result.status_code != 200:
-        return None
-    body = json.loads(result.text)
-    if body["data"]["after"] is not None:
-        newlist = word_list
-        if type(word_list[0]) is str:
-            temp = []
-            for i in word_list:
-                if not any(j['key'].lower() == i.lower() for j in temp):
-                    temp.append({"key": i.lower(), "count": 0, "times": 1})
-                else:
-                    item = list(filter(
-                                lambda search: search['key'] == i.lower(),
-                                temp))
-                    if len(item) > 0:
-                        item[0]["times"] = item[0]["times"] + 1
-            newlist = temp
-        for i in newlist:
-            for j in body["data"]["children"]:
-                for k in j["data"]["title"].lower().split():
-                    if i["key"] == k:
-                        i["count"] = i["count"] + 1
-        return count_words(subreddit, newlist, body["data"]["after"])
-    else:
-        newlist = word_list
-        if type(word_list[0]) is str:
-            temp = []
-            for i in word_list:
-                if not any(j['key'].lower() == i.lower() for j in temp):
-                    temp.append({"key": i.lower(), "count": 0, "times": 1})
-                else:
-                    item = list(filter(
-                                lambda search: search['key'] == i.lower(),
-                                temp))
-                    if len(item) > 0:
-                        item[0]["times"] = item[0]["times"] + 1
-            newlist = temp
-        for i in newlist:
-            for j in body["data"]["children"]:
-                for k in j["data"]["title"].lower().split():
-                    if i["key"] == k:
-                        i["count"] = i["count"] + 1
-        key = operator.itemgetter("key")
-        sorted_list = sorted(word_list, key=key)
-        key = operator.itemgetter("count")
-        sorted_list = sorted(sorted_list, key=key, reverse=True)
-        word_list = sorted_list
-        for i in sorted_list:
-            if i["count"] > 0:
-                print("{}: {}".format(i["key"], i["count"] * i["times"]))
+    params = {"after": after} if after else {}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code != 200:
+        print_counts(counts)
         return
+    
+    data = response.json()
+    children = data["data"]["children"]
+    for child in children:
+        title = child["data"]["title"].lower()
+        count_keywords(title, word_list, counts)
+    
+    after = data["data"]["after"]
+    if after:
+        count_words(subreddit, word_list, after, counts)
+    else:
+        print_counts(counts)
+
+
+def count_keywords(title, word_list, counts):
+    words = title.split()
+    for word in words:
+        word = word.strip('.,!?_')
+        if word.lower() in word_list:
+            counts[word.lower()] = counts.get(word.lower(), 0) + 1
+
+
+def print_counts(counts):
+    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    for word, count in sorted_counts:
+        print(f"{word}: {count}")
